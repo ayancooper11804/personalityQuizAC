d028d5ba551bf801d794349a0e657f7066f2220f
Ayan <ayancooper01@gmail.com>
2019-03-16 15:56:20 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+let franchise = null;
+let mHeroes = null
+let mVillains = null
+let dcHeroes = null
+let dcVillains = null
+
+let pageTitle = document.getElementById("Franchise/Type");
+let pageTitleText = pageTitle.innerHTML;	
+let tryAgain = document.getElementById("try-again");
+let quizWrapper = document.getElementById("quiz-Wrapper");
+let formSubmit = document.getElementById("submit");
+
+function processResults(){
+
+	console.log("this is the processResults function")
+
+}
+
+function getPersonality(){
+
+	console.log("get the personality")
+
+}
+
+function resetQuiz(){
+
+	console.log("reset the quiz")
+}
+
+tryAgain.addEventListener.click(resetQuiz);
+formSubmit.addEventListener.click(processResults);
+
+
